85116c1a5f888e7bc99433e42d98b1931e504f65
MiyuSato <83227352+MiyuSato@users.noreply.github.com>
2024-10-03 19:08:38 +0900

    Update setup.py

--- autoware_awsim_connect/setup.py
+++ autoware_awsim_connect/setup.py
@@ -1,4 +1,7 @@
+## launch fileを実行する場合はコメントアウトを解除
 from setuptools import find_packages, setup
+## import os 
+## from glob import glob
 
 package_name = 'autoware_awsim_connect'
 
@@ -10,6 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+       ## (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),  (launch)
     ],
     install_requires=['setuptools'],
     zip_safe=True,
